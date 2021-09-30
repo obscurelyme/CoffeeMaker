254baf10314666ca71244b3554b722cc4290722c
obscurelyme <nico.frozencoffee@gmail.com>
2021-09-29 19:52:18 -0700

    format

--- include/Game/Projectile.hpp
+++ include/Game/Projectile.hpp
@@ -2,31 +2,32 @@
 #define _projectile_hpp
 
 #include <SDL2/SDL.h>
-#include "Texture.hpp"
-#include <memory>
+
 #include <glm/glm.hpp>
+#include <memory>
 
 #include "Game/Collider.hpp"
+#include "Texture.hpp"
 
 class Projectile {
   public:
-    Projectile();
-    ~Projectile();
+  Projectile();
+  ~Projectile();
 
-    void Fire(float initialXPosition, float initialYPosition, double rotation);
-    void Reload();
-    void Update();
-    void Render();
+  void Fire(float initialXPosition, float initialYPosition, double rotation);
+  void Reload();
+  void Update();
+  void Render();
 
   private:
-    static std::shared_ptr<CoffeeMaker::Texture> _texture;
-    bool _fired;
-    SDL_FRect _clientRect;
-    double _rotation;
-    float _endX;
-    float _endY;
-    glm::vec2 _movement;
-    Collider* collider;
+  static std::shared_ptr<CoffeeMaker::Texture> _texture;
+  bool _fired;
+  SDL_FRect _clientRect;
+  double _rotation;
+  float _endX;
+  float _endY;
+  glm::vec2 _movement;
+  Collider* collider;
 };
 
 #endif
