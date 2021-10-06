7820b12737bb70e9561a492110ba49d5b1b9dafc
obscurelyme <nico.frozencoffee@gmail.com>
2021-10-05 20:31:39 -0700

    workflow

--- src/Cursor.cpp
+++ src/Cursor.cpp
@@ -10,19 +10,15 @@ using namespace CoffeeMaker;
 
 Cursor::Cursor(const std::string& filePath) : _cursor(nullptr) {
   SDL_Surface* surface = nullptr;
-  std::string cursorImgPath =
-      fmt::format("{}/{}/{}", CoffeeMaker::Utilities::AssetsDirectory(),
-                  "images", filePath);
+  std::string cursorImgPath = fmt::format("{}/{}/{}", CoffeeMaker::Utilities::AssetsDirectory(), "images", filePath);
   surface = IMG_Load(cursorImgPath.c_str());
   if (surface == nullptr) {
     Logger::Error("Could not load cursor surface");
-    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "IMG_Load returned NULLPTR",
-                             "Could not load cursor surface", NULL);
+    SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "IMG_Load returned NULLPTR", "Could not load cursor surface", NULL);
     exit(1);
   }
   SDL_SetColorKey(surface, SDL_TRUE,
-                  SDL_MapRGB(surface->format, CoffeeMaker::Texture::COLOR_KEY.r,
-                             CoffeeMaker::Texture::COLOR_KEY.g,
+                  SDL_MapRGB(surface->format, CoffeeMaker::Texture::COLOR_KEY.r, CoffeeMaker::Texture::COLOR_KEY.g,
                              CoffeeMaker::Texture::COLOR_KEY.b));
   _cursor = SDL_CreateColorCursor(surface, surface->w / 2, surface->h / 2);
   SDL_SetCursor(_cursor);
