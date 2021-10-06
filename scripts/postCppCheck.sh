f9b13ca4ba49af0b48469803a679e90a38a955be
obscurelyme <nico.frozencoffee@gmail.com>
2021-10-05 23:19:38 -0700

    run with shd

--- scripts/postCppCheck.sh
+++ scripts/postCppCheck.sh
@@ -4,7 +4,9 @@ numErrors=$(cat ./check.xml | grep "<error" | wc -l)
 
 if [ $numErrors -gt 0 ]
 then
+  echo `There were errors detected in the generated XML file. Please review check.xml in the artifacts for more information.`
   exit 1
 else
+  echo "No issues detected!"
   exit 0
 fi
