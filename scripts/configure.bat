aa622f878c9457fe238c712f049b329d74726241
obscurelyme <nico.frozencoffee@gmail.com>
2021-11-14 13:19:03 -0800

    quotes

--- scripts/configure.bat
+++ scripts/configure.bat
@@ -1,3 +1,3 @@
 @REM cmake --no-warn-unused-cli -DCMAKE_TOOLCHAIN_FILE:STRING=~/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -B build -S . -G "Visual Studio 16 2019" -T host=x64 -A x64
 
-cmake --no-warn-unused-cli -DCMAKE_TOOLCHAIN_FILE:STRING=~/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -B build -S . -G Ninja -DCMAKE_C_COMPILER:FILEPATH=C:\Program Files\LLVM\bin\clang.exe -DCMAKE_CXX_COMPILER:FILEPATH=C:\Program Files\LLVM\bin\clang++.exe
+cmake --no-warn-unused-cli -DCMAKE_TOOLCHAIN_FILE:STRING="~/vcpkg/scripts/buildsystems/vcpkg.cmake" -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -B build -S . -G Ninja -DCMAKE_C_COMPILER:FILEPATH="C:\Program Files\LLVM\bin\clang.exe" -DCMAKE_CXX_COMPILER:FILEPATH="C:\Program Files\LLVM\bin\clang++.exe"
