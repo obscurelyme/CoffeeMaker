4c74fb7991a587173a2f1cb0faddd241e6ea8273
obscurelyme <nico.frozencoffee@gmail.com>
2021-11-15 19:11:02 -0800

    with more options

--- .github/scripts/osx/configure.sh
+++ .github/scripts/osx/configure.sh
@@ -2,5 +2,9 @@
 
 cmake --no-warn-unused-cli \
   -DCMAKE_TOOLCHAIN_FILE="/usr/local/share/vcpkg/scripts/buildsystems/vcpkg.cmake" \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE \
+  -DCMAKE_BUILD_TYPE:STRING=Debug \
+  -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/clang \
+  -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/clang++ \
   -B ./build \
   -G Ninja
