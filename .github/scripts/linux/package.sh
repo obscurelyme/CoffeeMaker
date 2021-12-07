28d53b729d639e7cb4b34eb66beb646a43ce22a7
obscurelyme <nico.frozencoffee@gmail.com>
2021-12-06 21:47:10 -0800

    redo

--- .github/scripts/linux/package.sh
+++ .github/scripts/linux/package.sh
@@ -2,4 +2,5 @@
 
 mkdir release &&
   cp ./build/main ./release/ultra-cosmo-invaders &&
+    cp -r ./assets ./release/assets &&
   tar vcfz ultra-cosmo-invaders-linux.tar.gz ./release
--- .github/scripts/win/package.bat
+++ .github/scripts/win/package.bat
@@ -1,3 +1,4 @@
 mkdir release
 robocopy build/Debug release /E /XF "*CoffeeMakerUnitTests*"
+robocopy assets release/assets /E
 7z a -r ultra-cosmo-invaders-win.zip .\release\*
