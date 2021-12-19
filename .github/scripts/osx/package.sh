b7330f09f97a4352b47fb1e15f1344b695ca9d18
obscurelyme <nico.frozencoffee@gmail.com>
2021-12-19 10:38:04 -0800

    fix to osx package

--- .github/scripts/osx/package.sh
+++ .github/scripts/osx/package.sh
@@ -6,11 +6,11 @@ if [[ -d ./licensed-assets ]]; then
     cp ./build/main ./release/ultra-cosmo-invaders &&
     cp -r ./assets ./release/assets &&
     cp ./licensed-assets/images/*.png ./release/assets/images &&
-    tar vcfz ultra-cosmo-invaders-linux.tar.gz ./release
+    tar vcfz ultra-cosmo-invaders-osx.tar.gz ./release
 else
   echo "Licensed assets will NOT be packaged..." &&
     mkdir release &&
     cp ./build/main ./release/ultra-cosmo-invaders &&
     cp -r ./assets ./release/assets &&
-    tar vcfz ultra-cosmo-invaders-linux.tar.gz ./release
+    tar vcfz ultra-cosmo-invaders-osx.tar.gz ./release
 fi
