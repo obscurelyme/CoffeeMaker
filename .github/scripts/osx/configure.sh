8759e5fba739511aeebbb52656f69b54711137ad
obscurelyme <nico.frozencoffee@gmail.com>
2021-12-20 15:53:30 -0800

    clang 13

--- .github/scripts/osx/configure.sh
+++ .github/scripts/osx/configure.sh
@@ -5,7 +5,7 @@ cmake --no-warn-unused-cli \
   -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE \
   -DCMAKE_BUILD_TYPE:STRING=Release \
   -DCOFFEEMAKER_RELEASE_BUILD:BOOL=TRUE \
-  -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/clang \
-  -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/clang++ \
+  -DCMAKE_C_COMPILER:FILEPATH=/usr/local/opt/llvm/bin/clang \
+  -DCMAKE_CXX_COMPILER:FILEPATH=/usr/local/opt/llvm/bin/clang++ \
   -B ./build \
   -G Ninja
